c066110704a36b1efdd84c057a270c0c5c5e501e
simotae14 <startae14@gmail.com>
2020-05-01 15:59:02 +0200

    connected the app to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchField: ''
+      robots: []
     }
   }
   componentDidMount() {
@@ -18,21 +31,19 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ robots: users}))
   }
-  onSearchChange = (event) => {
-    this.setState({
-      searchField: event.target.value
-    });
-  }
   render() {
     const {
-      robots,
-      searchField
+      robots
     } = this.state;
+    const {
+      searchField,
+      onSearchChange
+    } = this.props;
     const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
     return !robots.length ?  <h1>Loading</h1> : (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -43,4 +54,4 @@ class App extends Component {
   }
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
