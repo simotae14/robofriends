3dfb6760bb44aece0f8af1b1a2d6ecf48a5483eb
simotae14 <startae14@gmail.com>
2020-05-02 19:35:59 +0200

    add async actions with redux thunk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers';
 import 'tachyons';
+
+import App from './containers/App';
+
+import * as serviceWorker from './serviceWorker';
+
+import { requestRobots, searchRobots } from './reducers';
+
+import './index.css';
 
 // createLogger
 const logger = createLogger();
+
+// create a root reducer
+const rootReducers = combineReducers({requestRobots, searchRobots});
+
 // create store
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
